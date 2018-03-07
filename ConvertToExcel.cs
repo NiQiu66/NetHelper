07b18f93cc6fd7709daabe8778450545ba34cf59
路通(lutong) <lulutongaili@gmail.com>
2018-03-07 10:55:47 +0800

    data to excel

---
+++ ConvertToExcel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+	public Class1()
+	{
+        var data =new List<Data>();
+
+        var workbook = new XSSFWorkbook();
+        var sheet = workbook.CreateSheet();
+
+        var row = sheet.CreateRow(0);
+        var cell = null as ICell;
+        var cellNum = 0;
+
+        #region title
+
+        row.CreateCell(cellNum++).SetCellValue("服务码");
+        row.CreateCell(cellNum++).SetCellValue("开始时间");
+        row.CreateCell(cellNum++).SetCellValue("结束时间");
+
+        cellNum = 0;
+        sheet.SetColumnWidth(cellNum++, 20 * 256);
+        sheet.SetColumnWidth(cellNum++, 20 * 256);
+        sheet.SetColumnWidth(cellNum++, 20 * 256);
+
+        #endregion
+        #region content
+        if (data != null && data.Any())
+        {
+            var i = 0;
+            foreach (var item in data)
+            {
+                cellNum = 0;
+                row = sheet.CreateRow(i + 1);
+                row.CreateCell(cellNum++).SetCellValue(item.ID);
+                row.CreateCell(cellNum++).SetCellValue(item.Name);
+                i++;
+            }
+        }
+        #endregion
+
+        var ms = new MemoryStream();
+        workbook.Write(ms);
+        return File(ms.ToArray(), "application/x-xls", $"{name}_服务码_{DateTime.Now.ToString("yyyyMMdd HHmmss")}.xlsx");
+    }
+
+    public class Data
+    {
+        public int ID;
+        public string Name;
+    }
+}
