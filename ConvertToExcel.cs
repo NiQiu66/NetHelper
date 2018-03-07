6110d154123269998db48adccff778178221a622
路通(lutong) <lulutongaili@gmail.com>
2018-03-07 10:59:29 +0800

    t

--- ConvertToExcel.cs
+++ ConvertToExcel.cs
@@ -18,12 +18,10 @@ public class Class1
 
         #region title
 
-        row.CreateCell(cellNum++).SetCellValue("服务码");
-        row.CreateCell(cellNum++).SetCellValue("开始时间");
-        row.CreateCell(cellNum++).SetCellValue("结束时间");
+        row.CreateCell(cellNum++).SetCellValue("ID");
+        row.CreateCell(cellNum++).SetCellValue("名字");
 
         cellNum = 0;
-        sheet.SetColumnWidth(cellNum++, 20 * 256);
         sheet.SetColumnWidth(cellNum++, 20 * 256);
         sheet.SetColumnWidth(cellNum++, 20 * 256);
 
@@ -45,7 +43,7 @@ public class Class1
 
         var ms = new MemoryStream();
         workbook.Write(ms);
-        return File(ms.ToArray(), "application/x-xls", $"{name}_服务码_{DateTime.Now.ToString("yyyyMMdd HHmmss")}.xlsx");
+        return File(ms.ToArray(), "application/x-xls", $"文件名.xlsx");
     }
 
     public class Data
